fe3a15a33584a6e50861edf44aa28caaa41ee6a3
Ishoediah <oeditishaan@gmail.com>
2026-06-14 13:32:51 -0300

    Structured every error object into one object named errors. Changed the error attribute to status. NOTE: doc_URL will show the link to the documentation for the specific error

--- src/core/errors.js
+++ src/core/errors.js
@@ -1,80 +1,84 @@
-// General Error
+const errors = {
 
-const error_ROUTE_NOT_FOUND = {
-    error: 404,
-    description: "Route not found",
+   //General Error
+
+   ROUTE_NOT_FOUND : { 
+    status: 404, 
+    description: "Route not found", 
     doc_URL: "",
     request_ID: "String"
-}
+    },
 
-// Internal Error
+   //Internal Error
 
-const error_INTERNAL = {
-    error: 500,
-    description: "Internal Server Error",
-    doc_URL: "",
+   INTERNAL : {
+    status: 500, 
+    description: "Internal Server Error", 
+    doc_URL: "", 
     request_ID: "String"
-}
+    },
 
-// Authentication errors
+   // Authentication errors
 
-const error_MISSING_API_KEY = {
-    error: 401,
-    description: "Missing API key, no auth provided",
-    doc_URL: "",
+   MISSING_API_KEY : {
+    status: 401, 
+    description: "Missing API key, no auth provided", 
+    doc_URL: "", 
     request_ID: "String"
-}
+    },
 
-const error_INVALID_API_KEY = {
-    error: 401,
+   INVALID_API_KEY : {
+    status: 401,
     description: "Invalid API key, not found or revoked",
     doc_URL: "",
     request_ID: "String"
-}
+    },
 
-const error_INVALID_PROXY_SECRET = {
-    error: 400,
+   INVALID_PROXY_SECRET : {
+    status: 403,
     description: "Bad Proxy Request",
     doc_URL: "",
     request_ID: "String"
-}
+    },
 
-// Rate/Usage errors
+    // Rate/Usage errors
 
-const error_OVER_MONTHLY_LIMIT = {
-    error: 429,
+   OVER_MONTHLY_LIMIT : {
+    status: 429,
     description: "Over monthly limit, too many requests",
     doc_URL: "",
     request_ID: "String"
-}
+    },
 
-const error_OVER_SECONDLY_LIMIT = {
-    error: 429,
+   OVER_SECONDLY_LIMIT : {
+    status: 429,
     description: "Too many requests per second",
     doc_URL: "",
     request_ID: "String"
-}
+    },
+    
+    // File errors
 
-// File errors
-
-const error_NO_FILE_PROVIDED = {
-    error: 400,
+   NO_FILE_PROVIDED : {
+    status: 400,
     description: "Bad request, no file provided",
     doc_URL: "",
     request_ID: "String"
-}
+    },
 
-const error_FILE_TO_LARGE = {
-    error: 400,
+   FILE_TOO_LARGE : {
+    status: 413,
     description: "Payload size too large",
     doc_URL: "",
     request_ID: "String"
-}
-const error_UNSUPPORTED_FILE_TYPE = {
-    error: 415,
+    },
+
+   UNSUPPORTED_FILE_TYPE : {
+    status: 415,
     description: "Unsupported Media Type",
     doc_URL: "",
     request_ID: "String"
+    }
 }
 
 // Helper function
