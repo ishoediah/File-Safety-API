0372a52b32f068a418148279f7057068d84586f9
Ishoediah <oeditishaan@gmail.com>
2026-06-23 22:53:23 -0300

    Created middleware for validating api key's from headers by extracting and comparing. NOTE: marketplace key's get generated by the marketplace, so when a marketplace api is detected it skips the validation process.

---
+++ src/middleware/auth.js
@@ -0,0 +1,29 @@
+import "dotenv/config"
+import { errors } from "../core/errors.js"; 
+import { returnError } from "../core/errors.js";
+import {hashedKeyLookup} from "../db/keys.js";
+import { createMiddleware } from "hono/factory";
+
+export const auth = createMiddleware(async (c, next) => {
+
+    const capturedHeader = c.req.header('Authorization')
+
+    if (c.req.header('X-RapidAPI-Proxy-Secret') == process.env.RAPIDAPI_PROXY_SECRET) {
+        await next()
+        return;
+    }else if( capturedHeader == null ) {
+        return returnError(c, errors.MISSING_API_KEY);
+    }
+    
+    const strippedHeader = capturedHeader.replace("Bearer ", "")
+
+    const headerLookup = await hashedKeyLookup(strippedHeader)
+
+    if (headerLookup == null){
+        return returnError(c, errors.INVALID_API_KEY)
+    } else { 
+        c.set('customer', headerLookup)
+        await next()
+    }
+})
+
