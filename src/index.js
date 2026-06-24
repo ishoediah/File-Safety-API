d679d870054d252ccd2fb0b3b5d9c20b47aa73e1
Ishoediah <oeditishaan@gmail.com>
2026-06-23 23:21:46 -0300

    Added middleware to route for testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
 import {serve} from "@hono/node-server"
+import {auth} from './middleware/auth.js'
 
 const PORT = 3000;
 
 const app = new Hono()
+
+app.use('/v1/*', auth)
+
+app.get('/v1/health', (c) => {
+    return c.json({status : "ok"})
+})
 
 serve({
     fetch: app.fetch,
@@ -12,6 +19,4 @@ serve({
     console.log(`App is running on port ${PORT}`)
 })
 
-app.get('/v1/health', (c) => {
-    return c.json({status : "ok"})
-})
+
