6c6b5c3f21b1363f5cbc5f3b4004540e02de0e71
Ishoediah <oeditishaan@gmail.com>
2026-06-24 15:57:26 -0300

    Added ratelimit to path. So first auth does its job and then traffic goes to ratelimit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import {serve} from "@hono/node-server"
 import {auth} from './middleware/auth.js'
+import { rateLimit } from "./middleware/rateLimit.js"
 
 const PORT = 3000;
 
 const app = new Hono()
 
 app.use('/v1/*', auth)
+app.use('/v1/*', rateLimit)
 
 app.get('/v1/health', (c) => {
     return c.json({status : "ok"})
