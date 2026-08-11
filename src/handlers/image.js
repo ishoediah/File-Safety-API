969c9cc648cb2fecbc260f9061b361e183b1fbea
Ishoediah <oeditishaan@gmail.com>
2026-08-11 08:52:35 -0300

    Created image handler

---
+++ src/handlers/image.js
@@ -0,0 +1,32 @@
+import sharp from 'sharp';
+
+sharp.block({ operation: ["VipsForeignLoadTiff", "VipsForeignLoadVips"] });
+
+async function sanitizeImage(buffer) {
+    const findings = []
+    let sanitized
+
+    try{
+
+        const metadata = await sharp(buffer).metadata()
+
+        if(metadata.exif) {
+            findings.push({ type: 'exif', action: 'removed EXIF metadata'})
+        }
+        if(metadata.iptc) {
+            findings.push({ type: 'iptc', action: 'removed IPTC metadata'})
+        }
+        if(metadata.xmp) {
+            findings.push({ type: 'xmp', action: 'removed XMP metadata'})
+        }
+
+        sanitized = await sharp(buffer).toBuffer()
+
+    } catch (err) {
+        return { sanitized: null, findings, error: true}
+    }
+
+    return {sanitized, findings}
+}
+
+export { sanitizeImage }
