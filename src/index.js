731e4d5c3aa8018aa2a4b4c429dd2415142034e9
Ishoediah <oeditishaan@gmail.com>
2026-08-15 12:56:20 -0300

    Added sanitizer route to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {serve} from "@hono/node-server"
 import {auth} from './middleware/auth.js'
 import { rateLimit } from "./middleware/rateLimit.js"
 import { fileGuard } from "./middleware/fileGuard.js";
+import { sanitize } from "./routes/sanitize.js";
 
 const PORT = 3000;
 
@@ -11,6 +12,8 @@ const app = new Hono()
 app.use('/v1/*', auth)
 app.use('/v1/*', rateLimit)
 app.use('/v1/*', fileGuard)
+
+app.post('/v1/sanitize', sanitize) //after middelware because middleware runs first
 
 app.get('/v1/health', (c) => {
     return c.json({status : "ok"})
