7a46a46cac70c2c5d4214715c0705dd39a94d4f8
Ishoediah <oeditishaan@gmail.com>
2026-09-09 22:36:01 -0300

    Fixed bug where svg file type does not get recognised as svg if the file starts with <xml

--- src/core/detectType.js
+++ src/core/detectType.js
@@ -3,14 +3,14 @@ import { fileTypeFromBuffer } from 'file-type';
 async function detectFileType(buffer) {
   const typeResult = await fileTypeFromBuffer(buffer);
 
-  if (typeResult) {
-    return typeResult.mime;
-  }
-
   const textContent = buffer.toString('utf-8');
 
   if (textContent.includes('<svg') || textContent.includes('xmlns="http://www.w3.org/2000/svg"')) {
     return 'image/svg+xml';
+  }
+
+  if (typeResult) {
+    return typeResult.mime;
   }
 
   if (textContent.includes(',') && textContent.split('\n').length > 1) {
